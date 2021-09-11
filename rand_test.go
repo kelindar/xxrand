9c2fe18fdc35f64418a5fd8836f9a77d03b74975
Roman Atachiants <roman.atachiants@gmail.com>
2021-09-11 16:47:48 +0400

    add tests

--- rand_test.go
+++ rand_test.go
@@ -8,25 +8,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUint32(t *testing.T) {
-	m := make(map[uint32]struct{})
-	for i := 0; i < 1e3; i++ {
-		n := Uint32()
-		if _, ok := m[n]; ok {
-			t.Fatalf("number %v already exists", n)
-		}
-		m[n] = struct{}{}
-	}
-}
-
-func TestUint32n(t *testing.T) {
+func TestUint64n(t *testing.T) {
 	const samples, bounds = 1000000, 100
-	for start := time.Now(); time.Since(start) < 1*time.Second; {
+	for start := time.Now(); time.Since(start) < 500*time.Millisecond; {
 
 		// Generate a distribution
 		m := make(map[int]float64)
 		for i := 0; i < samples; i++ {
-			n := Uint32n(bounds)
+			n := Uint64n(bounds)
 			if n >= bounds {
 				t.Fatalf("n > %v: %v", n, bounds)
 			}
@@ -43,12 +32,12 @@ func TestUint32n(t *testing.T) {
 			}
 		}
 
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(10 * time.Microsecond)
 	}
 }
 
 func TestBinary(t *testing.T) {
-	const samples = 1000
+	const samples = 10000
 
 	// Generate a distribution
 	m := make(map[int]float64)
@@ -71,4 +60,99 @@ func TestBinary(t *testing.T) {
 
 func TestNext(t *testing.T) {
 	assert.Greater(t, int(next()), 0)
+	assert.Greater(t, int(genericNext()), 0)
+}
+
+func TestIntn(t *testing.T) {
+	assert.Panics(t, func() {
+		Intn(-1)
+	})
+
+	assert.Less(t, Intn(10), 10)
+	assert.Less(t, Intn(1e12), int(1e12))
+}
+
+func TestInt31n(t *testing.T) {
+	assert.Panics(t, func() {
+		Int31n(-1)
+	})
+
+	assert.Less(t, int(Int31n(10)), 10)
+	assert.Less(t, int(Int31n(1e9)), int(1e9))
+}
+
+func TestInt63n(t *testing.T) {
+	assert.Panics(t, func() {
+		Int63n(-1)
+	})
+
+	assert.Less(t, int(Int63n(10)), 10)
+	assert.Less(t, int(Int63n(1e9)), int(1e9))
+}
+
+func TestInt32(t *testing.T) {
+	m := make(map[int32]struct{})
+	for i := 0; i < 1e3; i++ {
+		n := Int32()
+		if _, ok := m[n]; ok {
+			t.Fatalf("number %v already exists", n)
+		}
+		m[n] = struct{}{}
+	}
+}
+
+func TestInt63(t *testing.T) {
+	m := make(map[int64]struct{})
+	for i := 0; i < 1e3; i++ {
+		n := Int63()
+		if _, ok := m[n]; ok {
+			t.Fatalf("number %v already exists", n)
+		}
+		m[n] = struct{}{}
+	}
+}
+
+func TestUint32(t *testing.T) {
+	m := make(map[uint32]struct{})
+	for i := 0; i < 1e3; i++ {
+		n := Uint32()
+		if _, ok := m[n]; ok {
+			t.Fatalf("number %v already exists", n)
+		}
+		m[n] = struct{}{}
+	}
+}
+
+func TestUint64(t *testing.T) {
+	m := make(map[uint64]struct{})
+	for i := 0; i < 1e3; i++ {
+		n := Uint64()
+		if _, ok := m[n]; ok {
+			t.Fatalf("number %v already exists", n)
+		}
+		m[n] = struct{}{}
+	}
+}
+
+func TestBool(t *testing.T) {
+	var heads, tails int
+	for i := 0; i < 100; i++ {
+		if Bool() {
+			heads++
+		} else {
+			tails++
+		}
+	}
+	assert.NotZero(t, heads)
+	assert.NotZero(t, tails)
+}
+
+func TestFloat32(t *testing.T) {
+	assert.NotZero(t, Float32())
+	assert.NotZero(t, Float32())
+}
+
+func TestFloat64(t *testing.T) {
+	assert.NotZero(t, Float64())
+	assert.NotZero(t, Float64())
 }
